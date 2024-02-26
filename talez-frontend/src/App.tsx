a018a91240edd11e06ed8c7ed487d6ecf9e9ad0f
Shrey Koradia <shrey.koradia@yahoo.com>
2024-02-26 22:42:23 +0530

    authentication ui designing screen

--- talez-frontend/src/App.tsx
+++ talez-frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./assets/css/global.css";
+import Login from "./modules/auth/Login";
 import Signup from "./modules/auth/Signup";
 
 const App = () => {
   return (
     <>
-      <Signup />
+      <Routes>
+        <Route index path="signin" element={<Login />} />
+        <Route index path="signup" element={<Signup />} />
+      </Routes>
     </>
   );
 };
---
+++ talez-frontend/src/modules/auth/Login.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/shared/ui/ui/button";
+import styles from "../../assets/css/auth.module.css";
+import { Input } from "@/shared/ui/ui/input";
+import { Label } from "@radix-ui/react-label";
+
+const Login = () => {
+  return (
+    <>
+      <div className={styles.login_parent_container}>
+        <div className="grid w-[500px] gap-4">
+          <div className={styles.login_title_container}>
+            <p className="text-2xl font-semibold">
+              Join and streamline Workflows on <em>Talez</em>
+            </p>
+            <p className="text-[1rem] font-light">
+              Why worry when you can write<em>Talez</em>?.
+            </p>
+          </div>
+          <form>
+            <div className={styles.login_form_container}>
+              <Label className="sr-only" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="name@example.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={false}
+              />
+              <Input
+                id="password"
+                placeholder="Enter Password"
+                type="password"
+                autoCorrect="off"
+                disabled={false}
+              />
+              <Button disabled={false}>Sign in with Email</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
