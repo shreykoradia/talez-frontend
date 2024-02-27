9c37d4716de291184ee4e3ae1404d44f2cd1407c
Shrey Koradia <shrey.koradia@yahoo.com>
2024-02-27 21:55:37 +0530

    added tanstack query client

--- talez-frontend/src/App.tsx
+++ talez-frontend/src/App.tsx
@@ -2,14 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./assets/css/global.css";
 import Login from "./modules/auth/login/Login";
 import Signup from "./modules/auth/signup/Signup";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Routes>
-        <Route index path="signin" element={<Login />} />
-        <Route index path="signup" element={<Signup />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route index path="signin" element={<Login />} />
+          <Route index path="signup" element={<Signup />} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 };
