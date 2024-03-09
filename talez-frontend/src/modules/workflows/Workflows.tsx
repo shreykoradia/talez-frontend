0453cfcf90fd2eac5b1a02daf3a1beb2ddc5712e
Shrey Koradia <shrey.koradia@yahoo.com>
2024-03-09 15:24:32 +0530

    addition of header component

--- talez-frontend/src/modules/workflows/Workflows.tsx
+++ talez-frontend/src/modules/workflows/Workflows.tsx
@@ -1,9 +1,11 @@
 import styles from "@/assets/css/workflow.module.css";
+import Header from "@/shared/components/header/Header";
 import { Card, CardDescription, CardTitle } from "@/shared/ui/ui/card";
 
 const Workflows = () => {
   return (
     <>
+      <Header displayMore={false} displayCreate={true} />
       <div className={styles.workflow_parent_container}>
         <Card className="w-[300px]">
           <CardTitle>Talez</CardTitle>
--- talez-frontend/src/shared/components/header/Header.tsx
+++ talez-frontend/src/shared/components/header/Header.tsx
@@ -1,16 +1,41 @@
-import { History, Star } from "lucide-react";
+import { History, Plus, Star } from "lucide-react";
 
-const Header = () => {
+interface HeaderProps {
+  displayMore: boolean;
+  displayCreate: boolean;
+}
+
+const defaultProps = {
+  displayMore: true,
+  displayCreate: false,
+};
+
+const Header = ({
+  displayCreate = defaultProps.displayCreate,
+  displayMore = defaultProps.displayMore,
+}: HeaderProps) => {
   return (
     <>
       <div className="flex justify-between items-center p-4">
-        <p className="text-sm">Workflows</p>
-        <div className="display_more_container flex justify-evenly gap-2">
-          <p className="text-sm">Edited 1hr ago</p>
-          <p className="text-sm">Share</p>
-          <History />
-          <Star />
-        </div>
+        <p className="text-sm px-2 py-0.5 hover:bg-accent rounded-md">
+          Workflows
+        </p>
+        {displayMore ? (
+          <div className="display_more_container flex justify-evenly gap-2">
+            <p className="text-sm">Edited 1hr ago</p>
+            <p className="text-sm">Share</p>
+            <History />
+            <Star />
+          </div>
+        ) : null}
+        {displayCreate ? (
+          <div className="new_workflow_container">
+            <div className="flex gap-2 items-center hover:bg-accent px-2 py-0.5 rounded-md">
+              <Plus size={14} />
+              <p className="text-sm">Create Workflow</p>
+            </div>
+          </div>
+        ) : null}
       </div>
     </>
   );
