c9fe493163cb9b340eb43b54dcb63ffe53516a9f
Shrey Koradia <shrey.koradia@yahoo.com>
2024-05-19 17:12:57 +0530

    fixed talez toast for invite

--- talez-frontend/src/shared/hooks/useInviteUser.ts
+++ talez-frontend/src/shared/hooks/useInviteUser.ts
@@ -2,6 +2,7 @@ import { inviteUserRequestProps } from "./../types/index";
 import { useMutation } from "@tanstack/react-query";
 import useGetPeopleWithAccess from "./useGetPeopleWithAccess";
 import { inviteUser } from "../components/header/api/inviteUser";
+import { toast } from "../ui/ui/use-toast";
 
 const useInviteUser = (workflowId: string) => {
   const requestParams = {
@@ -12,7 +13,20 @@ const useInviteUser = (workflowId: string) => {
     mutationFn: (values: inviteUserRequestProps) =>
       inviteUser(values, requestParams),
     onSuccess: () => {
+      toast({
+        title: "Invite Added Successfully",
+        description:
+          "Fun Fact: Now Invited Memeber can also write talez on the workflow, hopefully if they know the Product",
+      });
       refetchPeopleWithAccessFn();
+    },
+    onError: () => {
+      toast({
+        title:
+          "Users having an account on talez are only being allowed to invited",
+        description:
+          "Currently we don't allow any external user to be a part of any workflows, for invites a member should atleast have account on talez and be verified.",
+      });
     },
   });
 
