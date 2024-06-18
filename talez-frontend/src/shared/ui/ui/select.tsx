682b344f9c1fe8faa245f3b3778b50f9b39ddb88
Shrey Koradia <shrey.koradia@yahoo.com>
2024-06-18 22:45:47 +0530

    fixed share popover

--- talez-frontend/src/shared/ui/ui/select.tsx
+++ talez-frontend/src/shared/ui/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm text-divamecha ring-offset-background placeholder:text-muted focus:outline-none focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-10 w-full items-center justify-between rounded-md border border-muted bg-background px-3 py-2 text-sm text-divamecha ring-offset-background placeholder:text-muted focus:outline-none focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
