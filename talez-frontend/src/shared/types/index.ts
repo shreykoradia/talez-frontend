65d6c217c24dcb787a4b720ab7075d913df4585c
Shrey Koradia <shrey.koradia@yahoo.com>
2024-07-14 12:50:35 +0530

    fixed version changes for input text

--- talez-frontend/src/shared/types/index.ts
+++ talez-frontend/src/shared/types/index.ts
@@ -33,11 +33,11 @@ export type ErrorResponse = AxiosError<AxiosErrorProps>;
 export interface peopleWithAccessResponse {
   _id: string;
   workflow: string;
-  shared_by: string;
+  sharedBy: string;
   role: string;
-  shared_at: string;
+  sharedAt: string;
   __v: number;
-  shared_to: SharedTo;
+  sharedTo: SharedTo;
 }
 export interface SharedTo {
   _id: string;
@@ -88,11 +88,11 @@ export interface sharedUserResponse {
 export interface SharedUsersEntity {
   _id: string;
   workflow: string;
-  shared_by: string;
+  sharedBy: string;
   role: string;
-  shared_at: string;
+  sharedAt: string;
   __v: number;
-  shared_to: SharedTo;
+  sharedTo: SharedTo;
 }
 export interface SharedTo {
   _id: string;
