3760a3fbd44f21c1e1f7fe53b521a7fe982019aa
Shrey Koradia <shrey.koradia@yahoo.com>
2024-07-30 09:03:39 +0530

    fixed links delelte functionality

--- talez-frontend/src/modules/attachments/Attachments.tsx
+++ talez-frontend/src/modules/attachments/Attachments.tsx
@@ -7,16 +7,31 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/shared/ui/ui/drawer";
-import { Link } from "lucide-react";
+import { Link, Trash2Icon } from "lucide-react";
 import CreateLinkModal from "./components/CreateLinkModal";
 import useGetLinks from "./hooks/useGetLinks";
+import { useMutation } from "@tanstack/react-query";
+import deleteLink from "./api/deleteLink";
+import Loader from "@/shared/components/loader/Loader";
 
 interface AttachementProp {
   selectedTale: string;
 }
 
 const Attachments = ({ selectedTale }: AttachementProp) => {
-  const { data } = useGetLinks(selectedTale || "");
+  const { data, refetch: getLinksRefetchFn } = useGetLinks(selectedTale || "");
+
+  const { mutate: deleteLinkFn, isPending: isDeletingLink } = useMutation({
+    mutationFn: (linkId: string) =>
+      deleteLink({ linkId: linkId, taleId: selectedTale }),
+    onSuccess: () => {
+      getLinksRefetchFn();
+    },
+  });
+
+  if (isDeletingLink) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -41,15 +56,23 @@ const Attachments = ({ selectedTale }: AttachementProp) => {
               </div>
             </div>
           </DrawerHeader>
-          <div className="flex items-center justify-start gap-4 flex-wrap p-4">
+          <div className="flex items-center justify-start flex-wrap p-4">
             {data?.data.links?.map((link, index) => (
-              <a
-                href={link?.linkUrl}
-                className="border border-input px-3 py-2 text-sm text-input rounded-lg hover:bg-secondary hover:transition hover:delay-200"
-                key={index}
-              >
-                <p>{link?.linkTitle}</p>
-              </a>
+              <div className="flex gap-2 items-center border border-input px-3 py-2 text-sm text-input rounded-lg hover:bg-secondary hover:transition hover:delay-200 ">
+                <a href={link?.linkUrl} key={index}>
+                  <p>{link?.linkTitle}</p>
+                </a>
+                <button
+                  className="p-2"
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteLinkFn(link?._id);
+                  }}
+                >
+                  <Trash2Icon size={14} />
+                </button>
+              </div>
             ))}
           </div>
         </DrawerContent>
